1d765dc435a614e4920bd174c7f5b7548a7db7ca
igi712 <igi712.mgj@gmail.com>
2024-11-28 19:57:52 +0700

    add api to profile screen (by jelang)

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,9 +1,12 @@
+import 'package:chatting_ui/services/profile_services.dart';
 import 'package:flutter/material.dart';
+
 import 'edit_profile_screen.dart';
 import 'landing_screen.dart';
 
+
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
@@ -11,6 +14,58 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   String name = "Boss Skibidi";
+  String email = "skibidi@sigma.com";
+  String role = "Owner";
+  bool isLoading = true;
+
+  final ProfileServices _profileServices = ProfileServices();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProfileData();
+  }
+
+  Future<void> _loadProfileData() async {
+    try {
+      final token = await _profileServices.getToken();
+      if (token == null) {
+        throw Exception('Token tidak ditemukan.');
+      }
+
+      final profileData = await _profileServices.fetchProfile(token);
+
+      setState(() {
+        name = profileData['username'];
+        email = profileData['email'];
+        role = profileData['tipe_user'];
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      _showErrorDialog('Gagal memuat profil. Pastikan Anda terhubung ke jaringan.');
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,25 +73,26 @@ class _ProfileScreenState extends State<ProfileScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: const Color(0xFF2C3E50),
-        iconTheme: IconThemeData(color: Colors.white),
-  leading: IconButton(
-    icon: Icon(Icons.arrow_back),
-    onPressed: () {
-      Navigator.pop(context);
-    },
-  ),
-  title: Text('Profile', style: TextStyle(color: Colors.white)), 
-  centerTitle: true,
-  actions: <Widget>[
-    PopupMenuButton<String>(
-      onSelected: (String result) {
-        switch (result) {
-          case 'Edit Profile':
-            Navigator.push(
+        iconTheme: const IconThemeData(color: Colors.white),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text('Profile', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (String result) {
+              switch (result) {
+                case 'Edit Profile':
+                  Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) => EditProfileScreen(
                         name: name,
+                        email: email,
                         onSave: (updatedName) {
                           setState(() {
                             name = updatedName;
@@ -45,176 +101,144 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       ),
                     ),
                   );
-            break;
-          case 'Log Out':
-            showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-      //title: Text('Konfirmasi'),
-      content: SingleChildScrollView(
-          child: Center(
-            child: Text.rich(
-              TextSpan(
-                text: '\nApakah kamu ingin ',
-                style: TextStyle(fontSize: 18),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: 'log out',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(text: '?'),
-                ],
-              ),
-            ),
-          ),
-        ),
-      actionsAlignment: MainAxisAlignment.spaceAround,
-      actions: <Widget>[
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop(); // Tutup dialog
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => LandingScreen()),
-            );
-          },
-          style: TextButton.styleFrom(
-            backgroundColor: Color(0xFF606D75),
-            foregroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(width: 9),
-              Container(
-                width: 24,
-                height: 24,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  shape: BoxShape.circle,
+                  break;
+                case 'Log Out':
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: const SingleChildScrollView(
+                          child: Center(
+                            child: Text.rich(
+                              TextSpan(
+                                text: '\nApakah kamu ingin ',
+                                style: TextStyle(fontSize: 18),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: 'log out',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  TextSpan(text: '?'),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        actionsAlignment: MainAxisAlignment.spaceAround,
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Tutup dialog
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => const LandingScreen()),
+                              );
+                            },
+                            style: TextButton.styleFrom(
+                              backgroundColor: const Color(0xFF606D75),
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
+                                SizedBox(width: 9),
+                                Icon(Icons.check, color: Colors.green),
+                                SizedBox(width: 8),
+                                Text('Ya'),
+                                SizedBox(width: 9),
+                              ],
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Tutup dialog
+                            },
+                            style: TextButton.styleFrom(
+                              backgroundColor: const Color(0xFF606D75),
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
+                                Icon(Icons.close, color: Colors.red),
+                                SizedBox(width: 8),
+                                Text('Tidak'),
+                              ],
+                            ),
+                          ),
+                        ],
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        ),
+                      );
+                    },
+                  );
+                  break;
+              }
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: 'Edit Profile',
+                child: ListTile(
+                  leading: Icon(Icons.edit),
+                  title: Text('Edit Profile'),
                 ),
-                child: Center(
-                  child: Icon(
-                    Icons.check,
-                    color: Colors.white,
-                    size: 16,
-                  ),
+              ),
+              const PopupMenuItem<String>(
+                value: 'Log Out',
+                child: ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text('Log Out'),
                 ),
               ),
-              SizedBox(width: 8),
-              Text('Ya'),
-              SizedBox(width: 9),
             ],
           ),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop(); // Tutup dialog
-          },
-          style: TextButton.styleFrom(
-            backgroundColor: Color(0xFF606D75),
-            foregroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: 24,
-                height: 24,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Icon(
-                    Icons.close,
-                    color: Colors.white,
-                    size: 16,
-                  ),
-                ),
-              ),
-              SizedBox(width: 8),
-              Text('Tidak'),
-            ],
-          ),
-        ),
-      ],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ],
       ),
-      );
-    },
-  );
-  break;
-        }
-      },
-      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-        const PopupMenuItem<String>(
-          value: 'Edit Profile',
-          child: ListTile(
-            leading: Icon(Icons.edit),
-            title: Text('Edit Profile'),
-          ),
-        ),
-        const PopupMenuItem<String>(
-          value: 'Log Out',
-          child: ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Log Out'),
-          ),
-        ),
-      ],
-    ),
-  ],
-),
-      body: Center(
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Container(
               width: 80,
               height: 80,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Colors.grey,
                 shape: BoxShape.circle,
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.person,
                 size: 50,
                 color: Colors.white,
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             Text(
-              '$name',
-              style: TextStyle(
-                fontSize: 22,
-              ),
+              name,
+              style: const TextStyle(fontSize: 22),
             ),
-            SizedBox(height: 5),
+            const SizedBox(height: 5),
             Text(
-              'skibidi@sigma.com',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey,
-              ),
+              email,
+              style: const TextStyle(fontSize: 14, color: Colors.grey),
             ),
-          SizedBox(height: 50),
-          Container(
-              padding: EdgeInsets.all(16),
+            const SizedBox(height: 50),
+            Container(
+              padding: const EdgeInsets.all(16),
               width: MediaQuery.of(context).size.width * 0.85,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(8),
-                boxShadow: [
+                boxShadow: const [
                   BoxShadow(
                     color: Color(0x3F000000),
                     blurRadius: 4,
@@ -226,35 +250,18 @@ class _ProfileScreenState extends State<ProfileScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(
-                    'Department Name',
-                    style: TextStyle(
-                      fontSize: 22,
-                   ),
-                  ),
-                  SizedBox(height: 10),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 5.0),
-                      child: Text(
-                        'Company Owner',
-                        style: TextStyle(
-                          color: Color(0xFF23272A),
-                        ),
-                      ),
-                    ),  
-                  SizedBox(height: 20),
-                  Text(
+                  const Text(
                     'Your Role',
                     style: TextStyle(
                       fontSize: 22,
                     ),
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   Padding(
                     padding: const EdgeInsets.only(left: 5.0),
                     child: Text(
-                      'Owner',
-                      style: TextStyle(
+                      role,
+                      style: const TextStyle(
                         color: Color(0xFF23272A),
                       ),
                     ),
@@ -264,7 +271,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
           ],
         ),
-      ),      
+      ),
     );
   }
 }
---
+++ lib/services/profile_services.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../helper/constant_app.dart';
+
+class ProfileServices {
+  final _storage = const FlutterSecureStorage();
+
+  Future<String?> getToken() async {
+    try {
+      return await _storage.read(key: 'auth_token');
+    } catch (e) {
+      throw Exception('Gagal membaca token: $e');
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchProfile(String token) async {
+    const url = AppConstants.BASE_URL + AppConstants.PROFILE;
+
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        return responseData['data'];
+      } else {
+        throw Exception('Gagal memuat data profil. Status: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error saat mengambil profil: $e');
+    }
+  }
+}
